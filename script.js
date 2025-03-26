e1d530703da0ff6a08d95e3c4ddba1ab1bebafa4
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-03-26 04:48:20 -0300

    fix: corrections

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-function abrirModal() {
-    const abrirModal = document.getElementById("confirmed")
-    if (abrirModal.classList.contains('hidden')) {
-        abrirModal.classList.remove('hidden')
-    }
-} 
+const baseModal = document.getElementById("container-base-modal")
+const cards = document.getElementById("cards")
+const container = document.getElementById("container")
 
-function fecharModal() {
-  const abrirModal = document.getElementById("confirmed")
 
-  if (abrirModal.classList.contains('modal')) {
-        abrirModal.classList.add('hidden')
+container.addEventListener("click", function(event) {
+  // console.log(event.target)
+
+  let parentButton = event.target.closest(".container-base")
+
+  if(parentButton) {
+    const name = parentButton.getAttribute("data-name")
+    const price = parentButton.getAttribute("data-price")
+
+    console.log(name)
+    console.log(price)
   }
-}
+})
+
+
+// function abrirModal() {
+//     const abrirModal = document.getElementById("confirmed")
+//     if (abrirModal.classList.contains('hidden')) {
+//         abrirModal.classList.remove('hidden')
+//     }
+// } 
+
+// function fecharModal() {
+//   const abrirModal = document.getElementById("confirmed")
+
+//   if (abrirModal.classList.contains('modal')) {
+//         abrirModal.classList.add('hidden')
+//   }
+// }
  
