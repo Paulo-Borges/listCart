0f220192e3e644c9ad18a44f84d189707f22203c
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-03-27 04:55:05 -0300

    fix: attempts to improve

--- script.js
+++ script.js
@@ -3,33 +3,33 @@ const cards = document.getElementById("cards")
 const container = document.getElementById("container")
 
 
-container.addEventListener("click", function(event) {
-  // console.log(event.target)
+// container.addEventListener("click", function(event) {
+//   // console.log(event.target)
 
-  let parentButton = event.target.closest(".container-base")
+//   let parentButton = event.target.closest(".container-base")
 
-  if(parentButton) {
-    const name = parentButton.getAttribute("data-name")
-    const price = parentButton.getAttribute("data-price")
+//   if(parentButton) {
+//     const name = parentButton.getAttribute("data-name")
+//     const price = parentButton.getAttribute("data-price")
 
-    console.log(name)
-    console.log(price)
-  }
-})
-
-
-// function abrirModal() {
-//     const abrirModal = document.getElementById("confirmed")
-//     if (abrirModal.classList.contains('hidden')) {
-//         abrirModal.classList.remove('hidden')
-//     }
-// } 
-
-// function fecharModal() {
-//   const abrirModal = document.getElementById("confirmed")
-
-//   if (abrirModal.classList.contains('modal')) {
-//         abrirModal.classList.add('hidden')
+//     console.log(name)
+//     console.log(price)
 //   }
-// }
+// })
+
+
+function abrirModal() {
+    const abrirModal = document.getElementById("confirmed")
+    if (abrirModal.classList.contains('hidden')) {
+        abrirModal.classList.remove('hidden')
+    }
+} 
+
+function fecharModal() {
+  const abrirModal = document.getElementById("confirmed")
+
+  if (abrirModal.classList.contains('modal')) {
+        abrirModal.classList.add('hidden')
+  }
+}
  
